67f01a641ae841e05e2e32838f6354016681d3e5
Hieu Tran <hieutran.th.358@gmail.com>
2023-04-24 20:36:40 +0300

    nvim: Merge new fixes from upstream kickstart.nvim c19fe7a

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -180,9 +180,7 @@ require('lazy').setup({
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
-    config = function()
-      pcall(require('nvim-treesitter.install').update { with_sync = true })
-    end,
+    build = ":TSUpdate",
   },
 
   -- Keep context at the top line when scrolling
