4704bd5a7cba598cd8b3bef2d0f87f01b29dccde
Hieu Tran <hieutran.th.358@gmail.com>
2023-09-20 20:16:29 +0300

    nvim/ftplugin/gitcommit.lua: Yank the changed file to '1' register

--- .config/nvim/ftplugin/gitcommit.lua
+++ .config/nvim/ftplugin/gitcommit.lua
@@ -1,3 +1,3 @@
--- copy the first changed file to the clipboard
+-- yank the first changed file to the '1' register
 vim.cmd('/Changes to be committed')
-vim.cmd.normal('jf:wy$1G')
+vim.cmd.normal('jf:w"1y$1G')
