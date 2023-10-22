90e5347a13a75e02bcc1ab65e01f86bfca47fa00
Hieu Tran <hieutran.th.358@gmail.com>
2023-10-23 02:54:22 +0300

    nvim: Comment for the terraform-vars filetype quick fix

--- .config/nvim/ftplugin/terraform-vars.lua
+++ .config/nvim/ftplugin/terraform-vars.lua
@@ -1,4 +1,4 @@
 -- Quick fix for 'E5248: Invalid character in group name' with *.tfvars file
 -- https://github.com/neovim/neovim/issues/23184
--- TODO: should be fixed after https://github.com/neovim/neovim/pull/24714 is merged
+-- TODO: should be fixed after https://github.com/neovim/neovim/pull/24714 is merged. It'll be in the next release (0.9.5 or 0.10.0, whichever comes first)
 vim.o.filetype = 'terraform'
