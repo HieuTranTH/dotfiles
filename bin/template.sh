6db24e0eece5193b546b03235f8ef6107d5c5010
Hieu Tran <hieutran.th.358@gmail.com>
2023-11-08 01:12:59 +0200

    bin/template.sh: Add logging functions

--- bin/template.sh
+++ bin/template.sh
@@ -11,12 +11,13 @@ readonly PROGNAME=${0##*/}
 
 ## Default values
 EXIT_FAILURE=1
-VERBOSE=0
+VERBOSE=${VERBOSE:-0}
 FILENAME=
 
 function usage() {
     echo -e "$PROGNAME - \n"
     echo -e "usage: $PROGNAME [-h] [-?]\n"
+    echo -e "   -v: Verbose mode."
     echo -e "   -h: Print this help message."
     echo -e "   -?: Print this help message."
 }
@@ -24,6 +25,29 @@ function usage() {
 function die() {
     echo "ERROR: $*" >&2
     exit $EXIT_FAILURE
+}
+
+function log {
+    [ ${VERBOSE} -eq 0 ] && return 0
+    local readonly level="$1"
+    local readonly message="$2"
+    local readonly timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    >&2 echo -e "${timestamp} [${level}] [$PROGNAME] ${message}"
+}
+
+function log_info {
+    local readonly message="$1"
+    log "INFO" "$message"
+}
+
+function log_warn {
+    local readonly message="$1"
+    log "WARN" "$message"
+}
+
+function log_error {
+    local readonly message="$1"
+    log "ERROR" "$message"
 }
 
 
