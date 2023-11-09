cb3afe432120516c011a4b453aac6335db082537
Hieu Tran <hieutran.th.358@gmail.com>
2023-11-09 11:44:53 +0200

    nvim/ftplugin/gitcommit.lua: Process only for COMMIT_EDITMSG buffer

--- .config/nvim/ftplugin/gitcommit.lua
+++ .config/nvim/ftplugin/gitcommit.lua
@@ -1,3 +1,5 @@
 -- yank the first changed file to the '1' register
-vim.cmd('/Changes to be committed')
-vim.cmd.normal('jf:w"1y$1G')
+if string.find(vim.api.nvim_buf_get_name(0), 'COMMIT_EDITMSG$') then
+  vim.cmd('/Changes to be committed')
+  vim.cmd.normal('jf:w"1y$1G')
+end
