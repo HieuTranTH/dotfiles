ec8e05df0fafc16a628c931269d97deb8bfbba41
Hieu Tran <hieutran.th.358@gmail.com>
2024-12-29 18:55:31 +0200

    nvim: Comment out auto settings for <help> filetype
    ZenMode doesn't work well with help pages.

--- .config/nvim/ftplugin/help.lua
+++ .config/nvim/ftplugin/help.lua
@@ -1,5 +1,5 @@
--- Open help in ZenMode and press q to exit
+--[[ -- Open help in ZenMode and press q to exit
 vim.cmd.ZenMode()
 vim.cmd.normal('zt')
 -- :bd to make sure opening the same file again will rerun this script
-vim.keymap.set('n', 'q', ':ZenMode<CR>:bd<CR>', { buffer = 0, desc = "[Q]uit help in ZenMode", silent = true})
+vim.keymap.set('n', 'q', ':ZenMode<CR>:bd<CR>', { buffer = 0, desc = "[Q]uit help in ZenMode", silent = true}) ]]
