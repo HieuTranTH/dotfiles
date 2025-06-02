efd01e14078386878b5e3614e04fd20435805a72
Hieu Tran <hieutran.th.358@gmail.com>
2025-06-02 08:39:22 +0300

    nvim/ftplugin/gitcommit.lua: Detect work item number in branch name

--- .config/nvim/ftplugin/gitcommit.lua
+++ .config/nvim/ftplugin/gitcommit.lua
@@ -1,5 +1,15 @@
--- yank the first changed file to the '1' register
 if string.find(vim.api.nvim_buf_get_name(0), 'COMMIT_EDITMSG$') then
+  -- automatic detect work item number in branch name
+  vim.cmd('/On branch')
+  local current_line = vim.api.nvim_get_current_line()
+  local number = current_line:match("(%d+)")
+  if number then
+    vim.api.nvim_buf_set_lines(0, 0, 0, false, { "" })
+    vim.api.nvim_buf_set_lines(0, 1, 1, false, { "" })
+    vim.api.nvim_buf_set_lines(0, 2, 2, false, { "Related work items: #" .. number })
+  end
+
+  -- yank the first changed file to the '1' register
   vim.cmd('/Changes to be committed')
   vim.cmd.normal('jf:w"1y$1G')
 end
