01a7cc83540033ac485d895b0ff9a31bbcb01d55
Hieu Tran <hieutran.th.358@gmail.com>
2026-05-28 16:02:20 +0300

    nvim: vim-fugitive: Create keymap to open Git in a new tab

--- .config/nvim/after/plugin/vim-fugitive.lua
+++ .config/nvim/after/plugin/vim-fugitive.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '<leader>g',  ':vertical rightbelow Git ', { desc = "[G]it command" })
+vim.keymap.set('n', '<leader>gt',  ':tab Git ', { desc = "[G]it command in new tab" })
 vim.keymap.set('n', '<leader>ge',  ':Gedit<cr>', { desc = "[G][e]dit" })
